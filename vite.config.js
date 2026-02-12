a97d88d719526b39f1b3d82343694e305b98f181
Jorge <jorge.acosta@upm.es>
2026-02-12 10:52:27 +0100

    solving paths

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath, URL } from "node:url";
 
+const githubRepoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const githubPagesBase =
+  process.env.GITHUB_ACTIONS && githubRepoName ? `/${githubRepoName}/` : null;
+const defaultBasePath = githubPagesBase || "/VANA/";
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/VANA/",
+  base: process.env.VITE_BASE_PATH || defaultBasePath,
   plugins: [react()],
   resolve: {
     alias: {
